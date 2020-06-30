3946ade42f3eb6cd38f47749e565dbea7fe801f2
taichunmin <taichunmin@gmail.com>
2020-07-01 04:38:56 +0800

    幫找不到加上 quickreply

--- line/view/nearby-post/not-found.js
+++ line/view/nearby-post/not-found.js
@@ -1,4 +1,15 @@
 module.exports = () => ({
   type: 'text',
-  text: '很抱歉，您上傳的位置附近沒有郵局',
+  text: '很抱歉，您上傳的位置附近沒有可以領取振興三倍券的場所，您可以點選下方「上傳新的定位」重新查詢。',
+  quickReply: {
+    items: [
+      {
+        type: 'action',
+        action: {
+          type: 'location',
+          label: '上傳新的定位',
+        },
+      },
+    ],
+  },
 })
